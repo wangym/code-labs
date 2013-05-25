7494d6f789b33c14112a293ed89201a831c7a48f
yumin <chinawym@gmail.com>
2013-05-25 16:56:00 +0800

    update

---
+++ labs/src/main/java/me/yumin/java/labs/test/TrapTest.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package me.yumin.java.labs.test;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author yumin
+ * 
+ */
+public class TrapTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+}
