e60c3ae66ce6f1a3d6e6c10e2d77c8878ff82516
yumin <chinawym@gmail.com>
2013-06-03 16:23:43 +0800

    update

--- java/src/test/java/me/yumin/java/labs/test/qrcode/ZXingTest.java
+++ java/src/test/java/me/yumin/java/labs/test/qrcode/ZXingTest.java
@@ -4,14 +4,14 @@
 package me.yumin.java.labs.test.qrcode;
 
 import com.google.zxing.BarcodeFormat;
-import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.EncodeHintType;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
-import com.google.zxing.qrcode.encoder.Encoder;
-import com.google.zxing.qrcode.encoder.QRCode;
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -21,22 +21,21 @@ import org.junit.Test;
 public class ZXingTest {
 
 	@Test
-	public void test自增陷阱() {
+	public void testZXingQRCode() {
 
 		boolean result = false;
 
 		try {
-			String content = "http://item.taobao.com/item.htm?id=12345&et_token=b794bb63cce11a79dd63153419271c4a&et_ver=1.0";
-			// 字符串1
-			QRCode qrCode = Encoder.encode(content, ErrorCorrectionLevel.H);
-			System.out.println(qrCode.toString());
-			// 图片版1
-			BitMatrix matrix = new MultiFormatWriter().encode(content, BarcodeFormat.QR_CODE, 120, 120);
-			MatrixToImageWriter.writeToFile(matrix, "png", new File("qr_code_MultiFormatWriter.png"));
-			// 图片版2
-			QRCodeWriter writer = new QRCodeWriter();
-			matrix = writer.encode(content, BarcodeFormat.QR_CODE, 230, 230);
-			MatrixToImageWriter.writeToFile(matrix, "png", new File("qr_code_QRCodeWriter.png"));
+			// 原始内容
+			String content = "你好, 中国!";
+			// 生成图片
+			Map<EncodeHintType, Object> hints = new HashMap<EncodeHintType, Object>();
+			hints = new HashMap<EncodeHintType, Object>();
+			hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.Q); // 容错率
+			hints.put(EncodeHintType.MARGIN, 0); // 边缘
+			hints.put(EncodeHintType.CHARACTER_SET, "GBK"); // 字符集
+			BitMatrix matrix = new QRCodeWriter().encode(content, BarcodeFormat.QR_CODE, 279, 279, hints);
+			MatrixToImageWriter.writeToFile(matrix, "png", new File("qr_code.png"));
 			// 最终结果
 			result = true;
 		} catch (Exception e) {
