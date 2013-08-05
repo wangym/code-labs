51a39f87caae04fae5dbd1c66c6697430e7e503e
yumin <chinawym@gmail.com>
2013-08-05 10:53:05 +0800

    update

--- shell/other/menu.sh
+++ shell/other/menu.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 #set -x
-echo -n "1.eticket@daily\n2.eticket@prod\n"
+echo -n "0:login1.cm3\n1:eticket@daily\n2:circuit@daily\n"
 read answer
-if [ $answer = 1 ]
+if [ $answer = 0 ]
+then
+	ssh xuanyin@login1.cm3.taobao.org
+elif [ $answer = 1 ]
 then
 	ssh xuanyin@10.232.101.107
 elif [ $answer = 2 ]
 then
-	ssh xuanyin@login1.cm3.taobao.org
+	ssh xuanyin@10.125.201.210
 else
 	echo "bye"
 	exit 0
