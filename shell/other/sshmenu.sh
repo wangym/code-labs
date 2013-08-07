135c8176b2098ecf8ba265abbefe191632059ad0
yumin <chinawym@gmail.com>
2013-08-07 09:40:08 +0800

    rename

--- shell/other/sshmenu.sh
+++ shell/other/sshmenu.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 #set -x
-echo -n "0:login1.cm3\n1:eticket@daily\n2:circuit@daily\n"
+echo -n "1:login1.cm3\n2:eticket@daily\n3:circuit@daily\n"
 read answer
-if [ $answer = 0 ]
+if [ $answer = 1 ]
 then
 	ssh xuanyin@login1.cm3.taobao.org
-elif [ $answer = 1 ]
+elif [ $answer = 2 ]
 then
 	ssh xuanyin@10.232.101.107
-elif [ $answer = 2 ]
+elif [ $answer = 3 ]
 then
 	ssh xuanyin@10.125.201.210
 else
