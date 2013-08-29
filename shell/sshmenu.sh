cfe0355473aa9712a908aafa50701b0e75e58d2a
yumin <chinawym@gmail.com>
2013-08-29 20:49:45 +0800

    update sshmenu

--- shell/sshmenu.sh
+++ shell/sshmenu.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #set -x
-echo -n "1:login1.cm3\n2:eticket@daily\n3:circuit@daily\n"
+echo -n "1:login1.cm3\n2:eticket@10.232.101.107\n3:circuit@10.125.201.210\n4:detail@10.235.144.147\n5:detail@10.235.144.148\n"
 read answer
 if [ $answer = 1 ]
 then
@@ -11,6 +11,12 @@ then
 elif [ $answer = 3 ]
 then
 	ssh xuanyin@10.125.201.210
+elif [ $answer = 4 ]
+then
+	ssh xuanyin@10.235.144.147
+elif [ $answer = 5 ]
+then
+	ssh xuanyin@10.235.144.148
 else
 	echo "bye"
 	exit 0
