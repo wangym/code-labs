5bfa6d7a3731cec85defb249a6c4ae40754262fe
yumin <chinawym@gmail.com>
2013-11-06 21:28:27 +0800

    update

--- java/src/test/java/me/yumin/java/labs/test/concurrency/SynchronizedRunnable.java
+++ java/src/test/java/me/yumin/java/labs/test/concurrency/SynchronizedRunnable.java
@@ -17,7 +17,7 @@ public class SynchronizedRunnable implements Runnable {
 	 */
 	public static synchronized void method(String name) {
 
-		for (int i = 0; i < 20; i++) {
+		for (int i = 0; i < 50; i++) {
 			System.out.println(String.format("[method@%s]%s", name, i));
 		}
 	}
@@ -28,7 +28,7 @@ public class SynchronizedRunnable implements Runnable {
 	public void struct(String name) {
 
 		synchronized (SynchronizedRunnable.class) {
-			for (int i = 0; i < 20; i++) {
+			for (int i = 0; i < 50; i++) {
 				System.out.println(String.format("[struct@%s]%s", name, i));
 			}
 		}
