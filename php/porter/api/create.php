3cff221cad80838d5791986d963590be16f9362f
yumin <chinawym@gmail.com>
2014-05-31 16:03:54 +0800

    update

--- php/porter/api/create.php
+++ php/porter/api/create.php
@@ -6,6 +6,9 @@ require_once($_SERVER['DOCUMENT_ROOT'] . 'include/bootstrap.php');
 
 /* 基础验证 */
 // 获取参数验证
+$params = api_sign_verify();
+
+/* 业务逻辑 */
 $redis = new RedisExt();
 $redis->flushdb();
 $ret = $redis->set("key", "value");
