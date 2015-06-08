9b154939044e42009c87a0ca011a7fa38272e2b6
yumin <chinawym@gmail.com>
2015-06-08 09:51:47 +0800

    update

---
+++ labs-java/src/test/java/me/yumin/java/labs/test/other/InvocationHandlerExample.java
@@ -0,0 +1,15 @@
+package me.yumin.java.labs.test.other;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * @author chinawym@gmail.com
+ * @since 2015-06-08
+ */
+public class InvocationHandlerExample implements InvocationHandler {
+
+    public Object invoke(Object o, Method method, Object[] objects) throws Throwable {
+        return null;
+    }
+}
