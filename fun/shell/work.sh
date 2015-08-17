ceedf00130cc35af63538035b13e3f8428f8f5e6
yumin <chinawym@gmail.com>
2015-08-17 14:52:32 +0800

    update sh

--- fun/shell/work.sh
+++ fun/shell/work.sh
@@ -3,6 +3,8 @@
 if [ "$1" = "alibaba" -o "$1" = "yumin" ]
 then
 	cp ~/.gitconfig.$1 ~/.gitconfig
+	cp ~/.ssh/id_rsa.$1 ~/.ssh/id_rsa
+	cp ~/.ssh/id_rsa.pub.$1 ~/.ssh/id_rsa.pub
 	cp ~/.m2/settings.xml.$1 ~/.m2/settings.xml
 	echo "work at $1 done"
 else
