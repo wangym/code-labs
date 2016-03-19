5ece8b90f3fa5bbffa3f2c2caf35a79c93925ce4
yumin <chinawym@gmail.com>
2016-03-19 20:37:40 +0800

    update clean.sh

--- fun/shell/clean.sh
+++ fun/shell/clean.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #set -x
 cd ~
-rm -rf ~/amsdata ~/forest ~/diamond ~/logs ~/pandora ~/configclient ~/vipsrv-logs
+rm -rf ~/amsdata* ~/forest ~/diamond ~/logs ~/pandora ~/configclient ~/vipsrv-logs
 rm antx.properties
 cd ~/alibaba/workspace/project/
 rm config.client.log* serviceConfig* ipmclient*
